9b68e50f32c001e9ad10a25448430b0253362b59
Gevorgyan George <43389097+Joker-7-7@users.noreply.github.com>
2026-01-30 15:28:11 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,23 @@
-// место для вашего кода
+#pragma once
+#include "geo.h"
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <optional>
+
+namespace catalogue
+{
+	class Transport {
+	public:
+		void AddRoute(const std::string& id,  const std::vector<std::string_view>& route);
+		void AddBusStop(const std::string& name, utils::Coordinates coord);
+		std::vector<std::string> SearchRoute(const std::string& id) const;
+		std::optional<utils::Coordinates> SearchBusStop(const std::string& name) const;
+		std::string GetRouteInfo(const std::string& name) const;
+		std::string GetBusesInfo(const std::string& stop) const;
+	private:
+		std::unordered_map<std::string, std::vector<std::string>> routes_;
+		std::unordered_map<std::string, utils::Coordinates> stops_;
+	};
+}
