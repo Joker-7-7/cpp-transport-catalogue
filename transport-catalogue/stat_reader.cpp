661cbb492547974b2b9b92d8851425eb4589284d
Gevorgyan George <43389097+Joker-7-7@users.noreply.github.com>
2026-01-30 16:20:53 +0300

    Added definition for stat reader

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,6 +1,7 @@
 #include "stat_reader.h"
 
 #include <string>
+#include <iostream>
 
 void reader::ParseAndPrintStat(const catalogue::Transport& transport_catalogue, std::string_view request,
                        std::ostream& output) {
@@ -12,5 +13,15 @@ void reader::ParseAndPrintStat(const catalogue::Transport& transport_catalogue, 
     }
     else if(request.substr(0, space_pos) == "Stop") {
         output << transport_catalogue.GetBusesInfo(std::string(request.substr(space_pos + 1))) << std::endl;
+    }
+}
+
+void reader::ReadRequests(std::istream& input, const catalogue::Transport& transport_catalogue, std::ostream& output) {
+    int stat_request_count;
+    input >> stat_request_count >> std::ws;
+    for (int i = 0; i < stat_request_count; ++i) {
+        std::string line;
+        getline(input, line);
+        reader::ParseAndPrintStat(transport_catalogue, line, output);
     }
 }
