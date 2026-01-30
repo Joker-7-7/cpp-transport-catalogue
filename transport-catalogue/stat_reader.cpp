45501a5c8b872503f76abb314b3ad9adfc792c96
Gevorgyan George <43389097+Joker-7-7@users.noreply.github.com>
2026-01-30 15:28:34 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,14 @@
-// место для вашего кода
+#include "stat_reader.h"
+
+#include <string>
+
+void reader::ParseAndPrintStat(const catalogue::Transport& transport_catalogue, std::string_view request,
+                       std::ostream& output) {
+    // Реализуйте самостоятельно
+    size_t spacaPos  = request.find(' ');
+    
+    if(request.substr(0, spacaPos) == "Bus")
+        output << transport_catalogue.GetRouteInfo(std::string(request.substr(spacaPos + 1))) << std::endl;
+    else if(request.substr(0, spacaPos) == "Stop")
+        output << transport_catalogue.GetBusesInfo(std::string(request.substr(spacaPos + 1))) << std::endl;
+}
