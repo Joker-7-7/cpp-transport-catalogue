ed1362cfb6dc0d4c295ac8f74dee28da587d87ad
Gevorgyan George <43389097+Joker-7-7@users.noreply.github.com>
2026-01-30 15:27:38 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,13 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+#include <ostream>
+
+#include "transport_catalogue.h"
+
+namespace reader
+{
+    void ParseAndPrintStat(const catalogue::Transport& transport_catalogue, std::string_view request,
+                        std::ostream& output);
+}
